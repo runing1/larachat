8b2da9e513fd071f51530f0ce8c062fb9cc435ff
zxm <503563951@qq.com>
2020-05-02 19:17:01 +0800

    'up'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        Gateway::$registerAddress='127.0.0.1:8282';
+        Gateway::$registerAddress='172.16.143.101:8282';
     }
 
     /**
